0ef483950de052d663d2df5eecb454b80c044ccd
Revanthsatyam <satyamrevanth@gmail.com>
2025-04-07 20:17:48 +0530

    changes

---
+++ shared-library/vars/nodejs.groovy
@@ -0,0 +1,35 @@
+def call() {
+  pipeline {
+
+    agent {
+      label 'workstation'
+    }
+
+    tools {
+      nodejs 'nodejs'
+    }
+
+    stage {
+
+      stage('Checkout') {
+        steps {
+          git branch: 'main', url: 'https://github.com/Revanthsatyam/catalogue.git'
+        }
+      }
+
+      stage('Compile & Build') {
+        steps {
+          sh "npm install"
+        }
+      }
+
+      stage('Test') {
+        steps {
+          sh "npm test"
+        }
+      }
+
+    }
+
+  }
+}
